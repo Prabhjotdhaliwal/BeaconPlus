bd955ed6e8e7e134b9cf0025c6f2f60419acbf75
Prabhjotdhaliwal <48627214+Prabhjotdhaliwal@users.noreply.github.com>
2021-10-13 15:14:49 -0400

    Update MainActivity.kt
    testing Github push

--- app/src/main/java/com/example/beaconplus/MainActivity.kt
+++ app/src/main/java/com/example/beaconplus/MainActivity.kt
@@ -10,7 +10,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("Hello Prabhjot")
         println("Hello Prabhjot")
-        println("Hello Prabhjot")
+        println("Testing Github push")
+
 
     }
 }
